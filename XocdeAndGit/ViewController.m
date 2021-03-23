b1523c6070f6bd338616a917c1f6ed689cb0219e
ghostkzshadow <kunzkun@163.com>
2021-03-23 20:59:44 +0800

    Initial Commit

---
+++ XocdeAndGit/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  XocdeAndGit
+//
+//  Created by KZ on 2021/3/23.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
